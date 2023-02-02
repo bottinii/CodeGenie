a440031e52b3388b75665fdc4381bae7a36003e0
Alessandro Bottini <124084966+bottinii@users.noreply.github.com>
2023-02-02 13:23:14 +0100

    Finished frame builder

--- src/assembler.py
+++ src/assembler.py
@@ -1,10 +1,10 @@
+
+""" This is the Assembler object.
+    It is necessary to store all the
+    data entered by the user and to create
+    the actual GUI """
+
 class AssemblerSettings:
-
-    """ This is the Assembler object.
-        It is necessary to store all the
-        data entered by the user and to create
-        the actual GUI """
-
     def __init__(self, title, geometry, resize, background):
         
         # Stored data
@@ -14,10 +14,9 @@ class AssemblerSettings:
         self.backgroundProject = background
 
         # Initialize settings in GUI code
-        with open("code.py", "w") as code:
-            code.write(f"""
+        with open("code.py", "w") as self.code:
+            self.code.write(f"""
 # Used libraries
-import tkinter
 import customtkinter
 
 # Class used to display the entire GUI
@@ -29,6 +28,33 @@ class Gui(customtkinter.CTk):
         self.title('{self.titleProject}')
         self.geometry('{self.geometryProject}')
         self.resizable({self.resizeProject})
-        self.configure(fg_color='{self.backgroundProject}')""")
+        self.configure(fg_color='{self.backgroundProject}')
+        self.grid_columnconfigure(0, weight=1)
+
+        # Widgets:
+        """)
             
-            code.close()
+            self.code.close()
+
+class AssemblerFrame:
+    def __init__(self, parent, name, size, background, border, radius, column, row, padx, pady):
+        
+        # Stored data
+        self.parentFrame = parent
+        self.nameFrame = name
+        self.sizeFrame = size.split('x')
+        self.backgroundFrame = background
+        self.borderFrame = border
+        self.radiusFrame = radius
+        self.columnFrame = column
+        self.rowFrame = row
+        self.padxFrame = padx
+        self.padyFrame = pady
+
+        # Add frame to the code
+        with open("code.py", "a") as self.code:
+            self.code.write(f"""
+        self.{self.nameFrame} = customtkinter.CTkFrame(master={self.parentFrame}, width={self.sizeFrame[0]}, height={self.sizeFrame[1]}, fg_color='{self.backgroundFrame}', border_width=3, border_color='{self.borderFrame}', corner_radius={self.radiusFrame})
+        self.{self.nameFrame}.grid(column={self.columnFrame}, row={self.rowFrame}, padx={self.padxFrame}, pady={self.padyFrame})""")   
+
+        self.code.close()     
--- src/widgets/frame.py
+++ src/widgets/frame.py
@@ -1,4 +1,5 @@
 import customtkinter
+from assembler import AssemblerFrame
 
 class Frame(customtkinter.CTk):
 
@@ -11,24 +12,29 @@ class Frame(customtkinter.CTk):
 
         # Window's settings
         self.title('CodeGenie - Frame')
-        self.geometry('550x300')
+        self.geometry('550x580')
         self.resizable(False, False)
 
         # Used to avoid writing repetitive code for the labels
         list_of_labels = [
-                            ['Frame\'s name', 1, 0],
-                            ['Frame\'s dimensions', 1, 1],
-                            ['Background color', 4, 0],
-                            ['Border color', 4, 1]
+                            ['Frame\'s parent', 1, 0],
+                            ['Frame\'s name', 1, 1],
+                            ['Frame\'s dimensions', 4, 0],
+                            ['Background color', 4, 1],
+                            ['Border color', 7, 0],
+                            ['Border radius', 7, 1],
+                            ['Column (y)', 10, 0],
+                            ['Row (x)', 10, 1],
+                            ['Padding x', 13, 0],
+                            ['Padding y', 13, 1]
                         ]
 
-        # First Spacer
-        self.spacer = customtkinter.CTkLabel(self, text='')
-        self.spacer.grid(row=0, column=0)
-
-        # Second Spacer
-        self.spacer = customtkinter.CTkLabel(self, text='')
-        self.spacer.grid(row=3, column=0)
+        # Spacers
+        c = 12
+        while c >= 0:
+            self.spacer = customtkinter.CTkLabel(self, text='')
+            self.spacer.grid(row=c, column=0)
+            c -= 3
 
         # Lables
         for i in range(len(list_of_labels)):
@@ -37,33 +43,85 @@ class Frame(customtkinter.CTk):
                                                 font=(None, 18))
             self.label.grid(row=list_of_labels[i][1], column=list_of_labels[i][2], pady=5, padx=30)
 
+        # Parent of the frame
+        self.parent_frame = customtkinter.CTkEntry(self,
+                                                placeholder_text='Example: self',
+                                                width=200,
+                                                corner_radius=10,
+                                                border_color='#5e45a5')
+        self.parent_frame.grid(row=2, column=0, padx=30)
+
         # Name of the frame
         self.name_frame = customtkinter.CTkEntry(self,
                                                 placeholder_text='Example: main_frame',
-                                                width=200, corner_radius=10,
+                                                width=200,
+                                                corner_radius=10,
                                                 border_color='#5e45a5')
-        self.name_frame.grid(row=2, column=0, padx=30)
+        self.name_frame.grid(row=2, column=1, padx=30)
 
         # Dimensions of the frame
         self.size_frame = customtkinter.CTkEntry(self,
                                                 placeholder_text='Example: 200x200',
-                                                width=200, corner_radius=10,
+                                                width=200,
+                                                corner_radius=10,
                                                 border_color='#5e45a5')
-        self.size_frame.grid(row=2, column=1, padx=30)
+        self.size_frame.grid(row=5, column=0, padx=30)
 
         # Background color of the frame
         self.background_frame = customtkinter.CTkEntry(self,
                                                 placeholder_text='Example: #2D9C7F',
-                                                width=200, corner_radius=10,
+                                                width=200,
+                                                corner_radius=10,
                                                 border_color='#5e45a5')
-        self.background_frame.grid(row=5, column=0, padx=30)
+        self.background_frame.grid(row=5, column=1, padx=30)
 
         # Border color of the frame
         self.border_frame = customtkinter.CTkEntry(self,
                                                 placeholder_text='Example: #B2721C',
-                                                width=200, corner_radius=10,
+                                                width=200,
+                                                corner_radius=10,
                                                 border_color='#5e45a5')
-        self.border_frame.grid(row=5, column=1, padx=30)
+        self.border_frame.grid(row=8, column=0, padx=30)
+
+        # Border radius
+        self.radius_frame = customtkinter.CTkEntry(self,
+                                                placeholder_text='Example: 20',
+                                                width=200,
+                                                corner_radius=10,
+                                                border_color='#5e45a5')
+        self.radius_frame.grid(row=8, column=1, padx=30)
+
+        # Column x
+        self.column_frame = customtkinter.CTkEntry(self,
+                                                placeholder_text='Example: 0',
+                                                width=200,
+                                                corner_radius=10,
+                                                border_color='#5e45a5')
+        self.column_frame.grid(row=11, column=0, padx=30)
+
+        # Row y
+        self.row_frame = customtkinter.CTkEntry(self,
+                                                placeholder_text='Example: 0',
+                                                width=200,
+                                                corner_radius=10,
+                                                border_color='#5e45a5')
+        self.row_frame.grid(row=11, column=1, padx=30)
+
+        # Padding x
+        self.padx_frame = customtkinter.CTkEntry(self,
+                                                placeholder_text='Example: 20',
+                                                width=200,
+                                                corner_radius=10,
+                                                border_color='#5e45a5')
+        self.padx_frame.grid(row=14, column=0, padx=30)
+
+        # Padding y
+        self.pady_frame = customtkinter.CTkEntry(self,
+                                                placeholder_text='Example: 20',
+                                                width=200,
+                                                corner_radius=10,
+                                                border_color='#5e45a5')
+        self.pady_frame.grid(row=14, column=1, padx=30)
 
         # Button to cancel frame creation
         self.frame_cancel = customtkinter.CTkButton(self,
@@ -77,7 +135,7 @@ class Frame(customtkinter.CTk):
                                                 font=(None, 15),
                                                 corner_radius=13,
                                                 command=lambda *args, **kwargs: self.destroy())
-        self.frame_cancel.grid(row=6, column=0, sticky='e', pady=40, padx=3)
+        self.frame_cancel.grid(row=15, column=0, sticky='e', pady=40, padx=3)
 
         # Button to save the frame
         self.frame_save_button = customtkinter.CTkButton(self,
@@ -89,5 +147,21 @@ class Frame(customtkinter.CTk):
                                                 width=100,
                                                 height=35,
                                                 font=(None, 15),
-                                                corner_radius=13)
-        self.frame_save_button.grid(row=6, column=1, sticky='w', pady=40, padx=3)
+                                                corner_radius=13,
+                                                command=self.save_frame)
+        self.frame_save_button.grid(row=15, column=1, sticky='w', pady=40, padx=3)
+
+    # Transfer the data to the assembler to add the frame to the code
+    def save_frame(self):
+        AssemblerFrame(parent=self.parent_frame.get(),
+                        name=self.name_frame.get(),
+                        size=self.size_frame.get(),
+                        background=self.background_frame.get(),
+                        border=self.border_frame.get(),
+                        radius=self.radius_frame.get(),
+                        column=self.column_frame.get(),
+                        row=self.row_frame.get(),
+                        padx=self.padx_frame.get(),
+                        pady=self.pady_frame.get())
+
+        return self.destroy()
